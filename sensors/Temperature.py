2e592668c3e7623d763178cfc467af6ba384590f
Robison Karls Custodio <robisonkarlscustodio@Robisons-MacBook-Pro.local>
2018-04-11 22:32:24 -0400

    added code for read the sensor for temperature

--- sensors/Temperature.py
+++ sensors/Temperature.py
@@ -1,6 +1,28 @@
 from random import randint
+import os
 
 class Temperature:
+
+    def __init__(self):
+        self.dsl8b20 = ''
+
     @staticmethod
-    def read():
+    def fake_read():
         return randint(-40,40)
+
+    def read(self):
+    #	global ds18b20
+        location = '/sys/bus/w1/devices/' + self.ds18b20 + '/w1_slave'
+        tfile = open(location)
+        text = tfile.read()
+        tfile.close()
+        secondline = text.split("\n")[1]
+        temperaturedata = secondline.split(" ")[9]
+        temperature = float(temperaturedata[2:])
+        temperature = temperature / 1000
+        return temperature
+
+    def setup(self):
+        for i in os.listdir('/sys/bus/w1/devices'):
+            if i != 'w1_bus_master1':
+                self.ds18b20 = i
