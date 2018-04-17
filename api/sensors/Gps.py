2af3c697b473bfe3a43c0735592503b6af4839aa
Robison.Custodio <Robison.Custodio@RCUSTODIO-P5520>
2018-04-17 09:47:42 -0400

    added the gps file

--- api/sensors/Gps.py
+++ api/sensors/Gps.py
@@ -13,7 +13,7 @@ class GpsPoller(threading.Thread):
     threading.Thread.__init__(self)
     global gpsd #bring it in scope
     gpsd = gps(mode=WATCH_ENABLE) #starting the stream of info
-    self.current_value = None
+    self.current_value = Nonegps
     self.running = True #setting the thread running to true
  
   def run(self):
@@ -32,26 +32,26 @@ if __name__ == '__main__':
       os.system('clear')
  
       print(' GPS reading')
-      print('----------------------------------------'
-      print 'latitude    ' , gpsd.fix.latitude
-      print 'longitude   ' , gpsd.fix.longitude
-      print 'time utc    ' , gpsd.utc,' + ', gpsd.fix.time
-      print 'altitude (m)' , gpsd.fix.altitude
-      print 'eps         ' , gpsd.fix.eps
-      print 'epx         ' , gpsd.fix.epx
-      print 'epv         ' , gpsd.fix.epv
-      print 'ept         ' , gpsd.fix.ept
-      print 'speed (m/s) ' , gpsd.fix.speed
-      print 'climb       ' , gpsd.fix.climb
-      print 'track       ' , gpsd.fix.track
-      print 'mode        ' , gpsd.fix.mode
-      print
-      print 'sats        ' , gpsd.satellites
+      print('----------------------------------------')
+      print('latitude    ' , gpsd.fix.latitude)
+      print('longitude   ' , gpsd.fix.longitude)
+      print('time utc    ' , gpsd.utc,' + ', gpsd.fix.time)
+      print('altitude (m)' , gpsd.fix.altitude)
+      print('eps         ' , gpsd.fix.eps)
+      print('epx         ' , gpsd.fix.epx)
+      print('epv         ' , gpsd.fix.epv)
+      print('ept         ' , gpsd.fix.ept)
+      print('speed (m/s) ' , gpsd.fix.speed)
+      print('climb       ' , gpsd.fix.climb)
+      print('track       ' , gpsd.fix.track)
+      print('mode        ' , gpsd.fix.mode)
+      print(' ')
+      print('sats        ' , gpsd.satellites)
  
       time.sleep(5) #set to whatever
  
   except (KeyboardInterrupt, SystemExit): #when you press ctrl+c
-    print "\nKilling Thread..."
+    print("\nKilling Thread...")
     gpsp.running = False
     gpsp.join() # wait for the thread to finish what it's doing
-  print "Done.\nExiting."
+  print("Done.\nExiting.")
