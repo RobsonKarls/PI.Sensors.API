6832c6093c8e5bd258c92e7d6277905fc97196d1
Robison Karls Custodio <robisonkarlscustodio@Robisons-MacBook-Pro.local>
2018-04-20 10:28:58 -0400

    fix

---
+++ api/sensors/Flame.py
@@ -0,0 +1,21 @@
+import PCF8591 as ADC
+import RPi.GPIO as GPIO
+import time
+import math
+
+class Flame:
+
+    def __init__(self):
+        GPIO.setmode(GPIO.BCM)
+        self.DO = 17
+        self.setup()
+
+    def setup(self):
+        ADC.setup(0x48)
+        GPIO.setup(self.DO, GPIO.IN)
+
+    def read(self):
+        print ADC.read(0)
+        tmp = GPIO.input(self.DO)
+        return tmp
+
